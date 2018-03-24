f54c851eb6f5ee0c3376d1f5007ad29751a16b24
Roman Draguntsov <draguntsow@yahoo.com>
2018-03-25 00:57:52 +0200

    Documentation added

--- loader.py
+++ loader.py
@@ -1,7 +1,24 @@
+'''
+Main package of the instagloader.
+Allows to download all photos from the page at the moment.
+
+TODO:
+Create a console-launch mode (maybe in other package?);
+More precise and scalable loading settings are needed;
+Direct load the html-page in-box needed (the authentication support will be required);
+Verbose mode also will be helpful;
+More than 20 photos per one loading is neccessary;
+
+BUG:
+Last one or two downloaded photos are not presented on the page given,
+but exist in code and, of course, are downloaded by the utility.
+'''
+
 import re
 import urllib.request
 import os
 
+#Scanning document function for finding direct photos URLs
 def scan(filename):
     with open(filename) as html:
         raw = html.read()
@@ -14,9 +31,10 @@ def scan(filename):
 
     return links
 
+#Loading function is used for download photos by links retrieved from scan()
 def load(links, fold=''):
     if fold == '':
-        fold = os.getcwd()
+        fold = os.getcwd() #if the directiry is not specified, the current one will be used
 
     index = 1
     for link in links:
