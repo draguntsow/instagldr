dea875fab9deeefc492e3949094dd64247f86aa1
Roman Draguntsov <draguntsow@yahoo.com>
2018-03-25 00:21:48 +0200

    Start

---
+++ loader.py
@@ -0,0 +1,27 @@
+import re
+import urllib.request
+import os
+
+def scan(filename):
+    with open(filename) as html:
+        raw = html.read()
+
+    link_pattern = r'display_url":".*?.jpg"'
+    links = re.findall(link_pattern, raw, re.MULTILINE)
+
+    for i in range(len(links)):
+        links[i] = links[i][14:(len(links[i])-1)]
+
+    return links
+
+def load(links, fold=''):
+    if fold == '':
+        fold = os.getcwd()
+
+    index = 1
+    for link in links:
+        urllib.request.urlretrieve(link, fold+'\\'+str(index)+'.jpg')
+        index+=1
+
+lnks = scan(r'E:\DEVELOPMENT\Python\INSTATOOL\sample.html')
+load(lnks, r'E:\DEVELOPMENT\Python\INSTATOOL')
